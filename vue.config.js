bdd2fe448be78f67a068306773c8d7f81d88e4e9
dwinters <dwinters@mitre.org>
2021-05-10 14:31:52 -0400

    Updating babel/webpack settings to support IE11.

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,34 @@
 const path = require('path');
 
 module.exports = {
+  // chainWebpack: config => {
+  //   // disable cache for prod only, remove the if to disable it everywhere
+  //   // if (process.env.NODE_ENV === 'production') {
+  //     config.module.rule('vue').uses.delete('cache-loader');
+  //     config.module.rule('js').uses.delete('cache-loader');
+  //     config.module.rule('ts').uses.delete('cache-loader');
+  //     config.module.rule('tsx').uses.delete('cache-loader');
+  //   // }
+  // },
+  transpileDependencies: [
+    // can be string or regex
+    'fhirclient',
+    'survey-vue',
+    'cql-execution',
+    'cql-exec-fhir',
+    'questionnaire-to-survey',
+    'vue',
+    'vue-loader',
+    'vue-loader-v16',
+    'vue-style-loader',
+    'vue-template-compiler',
+    'vue-template-es2015-compiler',
+    'sax',
+    'xmldoc',
+    'body-parser',
+    'debug',
+    '@lhncbc/ucum-lhc'
+  ],
   configureWebpack: {
     module: {
       rules: [
@@ -26,7 +54,19 @@ module.exports = {
           }
         },
         {
+          test: /CqlProcessor\.js$/,
+          use: ['babel-loader']
+        },
+        {
+          test: /main\.js$/,
+          use: ['babel-loader']
+        },
+        {
           test: /\.js\.map$/,
+          use: ['ignore-loader']
+        },
+        {
+          test: /\.0\.0\.xml\.js$/,
           use: ['ignore-loader']
         }
       ]
