f90511755ff97dabd0f31e31d17e071f4e142975
Amy Chen <amysbubble@Amys-MacBook-Pro-3.local>
2022-10-18 10:34:01 -0700

    custon theme - fetch env data

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,9 +2,10 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css';
 import themes from '../context/themes';
-import {getEnv} from '../util/util';
+import {fetchEnvData, getEnv} from '../util/util';
 
 const getAppTheme = () => {
+    fetchEnvData();
     const projectID = String(getEnv("VUE_APP_PROJECT_ID")).toLowerCase();
     if (themes[projectID] && themes[projectID].app) return themes[projectID].app;
     return themes["default"].app;
